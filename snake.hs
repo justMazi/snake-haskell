b70461ecc323a067d2d3df9ec2719c15478b75ff
Mazi <martin.mazac1@gmail.com>
2024-06-13 19:59:19 +0200

    Add directions

--- snake.hs
+++ snake.hs
@@ -17,3 +17,5 @@ data GameState = GameState
     wantedNewDirection :: Direction,
     getScore :: Score
   }
+
+directionVectors = fromList [(UP, (0, -1)), (DOWN, (0, 1)), (LEFT, (-1, 0)), (RIGHT, (1, 0))]
